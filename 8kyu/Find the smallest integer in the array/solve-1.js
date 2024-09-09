51f85b80ce20606dcb2ac221ac7f8d4fcd75d1ac
UMAtaullin <ataullinural@gmail.com>
2024-09-09 14:40:14 +0500

    feat: initial implementation of findSmallest Int function
    - implemented a function to find the smallest integer in an array.
    - the function iterates through the array and compares each element.

---
+++ 8kyu/Find the smallest integer in the array/solve-1.js
@@ -0,0 +1,11 @@
+function findSmallestInt(arr) {
+  let smallest = arr[0]
+  for (let i = 1; i <= arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i]
+    }
+  }
+  return smallest
+}
+
+console.log(findSmallestInt([34, 345, -1, -100]))
