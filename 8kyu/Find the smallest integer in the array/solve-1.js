3b695c59bae14ca0fb012cf7ca2381e67e91d1fc
UMAtaullin <ataullinural@gmail.com>
2024-09-10 18:24:22 +0500

    fix: corrected loop condition in findSmallestInt function
    - Changed loop condition from `i <= arr.length` to `i < arr.length` to prevent accessing an undefined element.

--- 8kyu/Find the smallest integer in the array/solve-1.js
+++ 8kyu/Find the smallest integer in the array/solve-1.js
@@ -1,6 +1,6 @@
 function findSmallestInt(arr) {
   let smallest = arr[0]
-  for (let i = 1; i <= arr.length; i++) {
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] < smallest) {
       smallest = arr[i]
     }
