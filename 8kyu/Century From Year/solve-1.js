e9e81ceca5588348b681bc4e540b245f1dd8855d
UMAtaullin <ataullinural@gmail.com>
2024-09-20 11:46:22 +0500

    using Math.ceil() for js and integer division for python

---
+++ 8kyu/Century From Year/solve-1.js
@@ -0,0 +1,10 @@
+function century(year) {
+  return Math.ceil(year / 100)
+}
+
+// function century(year) {
+//   return Math.floor(year // 100)
+// }
+
+console.log(century(1011))
+
---
+++ 8kyu/Century From Year/solve-1.py
@@ -0,0 +1,4 @@
+def century(year):
+  return (year + 99) // 100
+  
+print(century(1701))
