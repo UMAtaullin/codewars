e3cb56a430bc188c0e0fe179d5f56bf230d6b10f
UMAtaullin <ataullinural@gmail.com>
2024-10-06 11:44:23 +0500

    refactor: code style better

--- 8kyu/Sum of positive/solve-3.js
+++ 8kyu/Sum of positive/solve-3.js
@@ -1,5 +1,7 @@
 function positiveSum(arr) {
-  return arr.filter(num=>num >0).reduce((total, num) => total + num, 0)
+  return arr
+    .filter(el => el > 0)
+    .reduce((acc, curr) => acc + curr, 0)
 }
 
 console.log(positiveSum([1, 2, 3, -5, 4, 5])); // 15
